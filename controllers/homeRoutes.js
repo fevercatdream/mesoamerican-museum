7865f22393ba8108e09566f90ce300850a9495be
Ian Danas <ianmdanas@gmail.com>
2023-05-06 13:03:20 -0700

    testing handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     //change this res.send to be res.render and then the name of the views file
-    res.send('this is the homepage');
+    res.render('index');
   });
 
 
