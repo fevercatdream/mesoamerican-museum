528c30e54aed59af3e4b981a258aba9a5d91ca39
fevercatdream <kuma18@uw.edu>
2023-05-07 01:24:11 -0700

    update comments in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,13 @@ var modalLoginLink = document.querySelector("#modal-login-link");
 var modalCreateAccountBtn = document.querySelector("#modal-create-account-btn");
 var modalSubmitBtn = document.querySelector("#modal-submit-btn");
 
-
-// add class is-active to show modal
+// function to show modal
 function openModal() {
 //   modalEl.classList.add("is-active");
     modalEl.showModal();
 }
-// remove class is-active to hide modal
+
+// function to close modal
 function closeModal(event) {
 //   modalEl.classList.remove("is-active");
     if (event.target === modalEl) {
@@ -19,17 +19,20 @@ function closeModal(event) {
 }
 
 // ------------------event listeners--------------------
+
 // open modal
 modalLoginLink.addEventListener("click", openModal);
 
 // close modal
 modalEl.addEventListener("click", closeModal);
 
+// submit button
 modalSubmitBtn.addEventListener("click", function (event) {
   event.preventDefault();
- 
+
 });
 
+// create account button
 modalCreateAccountBtn.addEventListener("click", function (event) {
   event.preventDefault();
   
