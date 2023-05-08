0ccb00c9c3f552a2333cab7cb927a72e8a4b6763
Ian Danas <ianmdanas@gmail.com>
2023-05-07 21:46:33 -0700

    adding links to home from museum icon

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 // Attach an event listener to the login form
 const loginForm = document.getElementById('login-form');
 var modalCreateAccountBtn = document.querySelector("#modal-create-account-btn");
+var modalCreateMusAccountBtn = document.querySelector("#modal-em-login-link");
 
 
 modalSubmitBtn.addEventListener("click", async function (event) {
@@ -13,7 +14,7 @@ modalSubmitBtn.addEventListener("click", async function (event) {
     headers: {'Content-Type': 'application/json' },
     body: JSON.stringify({username, password }),
   })
-  if (response.ok) {
+  if (response.ok) { 
     window.location.href = '/additem';
   } else {
     const error = await response.json();
@@ -40,3 +41,39 @@ modalCreateAccountBtn.addEventListener("click", async function (event) {
   
 });
 
+modalSubmitBtn.addEventListener("click", async function (event) {
+  event.preventDefault();
+  console.log('pressed submit button')
+  const username = document.getElementById('model-em-login-id').elements["username"].value
+  const password = document.getElementById('model-em-login-id').elements["password"].value
+  const response = await fetch('/api/museum/login', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json' },
+    body: JSON.stringify({username, password }),
+  })
+  if (response.ok) {
+    window.location.href = '/additem';
+  } else {
+    const error = await response.json();
+    alert(error.message);
+  }
+})
+modalCreateAccountBtn.addEventListener("click", async function (event) {
+  event.preventDefault();
+  console.log('pressed create account btn')
+  const username = document.getElementById('model-em-login-id').elements["username"].value
+  const password = document.getElementById('model-em-login-id').elements["password"].value
+  const response = await fetch('/api/musuem/', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json' },
+    body: JSON.stringify({username, password}),
+  })
+  if (response.ok) {
+   console.log('created user')
+  } else {
+    const error = await response.json();
+    alert(error.message);
+  }
+
+  
+});
